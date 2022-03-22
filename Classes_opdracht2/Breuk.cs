02ecd7e994010ab5f6a8b15877e22088795eb6ac
Thibe Provost <thibe.provost@viso-cor-mariae.be>
2022-03-22 09:36:39 +0100

    get

--- Classes_opdracht2/Breuk.cs
+++ Classes_opdracht2/Breuk.cs
@@ -6,38 +6,49 @@ namespace Classes_opdracht2
 {
     class Breuk
     {
-        public int intTeler;
-        public int intNoemer;
-        
+        private int intTeler;
+        private int intNoemer;
+
+        public int INTTELLER
+        {
+            get { return intTeler; }
+            set { intTeler = value; }
+        }
+        public int INTNOEMER
+        {
+            get { return intNoemer; }
+            set { intNoemer = value; }
+        }
+
         public Breuk()
         {
             intTeler = 4;
             intNoemer = 2;            
         }
 
-        public Breuk(Breuk NBreuk)
+        /*public Breuk(Breuk NBreuk)
         {
             Console.Write("Geef de Teller: ");
-            intTeler = Convert.ToInt32(Console.ReadLine());
+            NBreuk.INTTELLER = Convert.ToInt32(Console.ReadLine());
             Console.Write("Geef de Noemer: ");
-            intNoemer = Convert.ToInt32(Console.ReadLine());
-        }
+            NBreuk.INTNOEMER = Convert.ToInt32(Console.ReadLine());
+        }*/
         public void Get(Breuk NBreuk)
         {
-            Console.WriteLine($"De Teller is: {intTeler}");
-            Console.WriteLine($"De Noemer is: {intNoemer}");
+            Console.WriteLine($"De Teller is: {NBreuk.INTTELLER}");
+            Console.WriteLine($"De Noemer is: {NBreuk.INTNOEMER}");
         }
         public void Aanpassen(Breuk NBreuk)
         {
-            Console.WriteLine("Geef de Teller: ");
-            NBreuk.intTeler = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Geef de Noemer: ");
-            NBreuk.intNoemer = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Geef de Teller: ");
+            NBreuk.INTTELLER = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Geef de Noemer: ");
+            NBreuk.INTNOEMER = Convert.ToInt32(Console.ReadLine());
         }
         public double quotiënt(Breuk NBreuk)
         {
             double dblQuotiënt;
-            dblQuotiënt = Convert.ToDouble(NBreuk.intTeler / NBreuk.intNoemer);
+            dblQuotiënt = Convert.ToDouble(NBreuk.INTTELLER / NBreuk.INTNOEMER);
             return dblQuotiënt;
         }
     }
--- Classes_opdracht2/Program.cs
+++ Classes_opdracht2/Program.cs
@@ -6,7 +6,31 @@ namespace Classes_opdracht2
     {
         static void Main(string[] args)
         {
-            Breuk NBreuk = new Breuk();
+            Breuk SBreuk = new Breuk();
+            string strAnswer;
+            double dblQuotiënt;
+            Console.Write("Wilt u zelf een breuk ingeven of de standaar 4/2? (Z/S) ");
+            strAnswer = Console.ReadLine().ToUpper();
+
+            if (strAnswer == "Z")
+            {
+                Breuk NBreuk = new Breuk();
+                NBreuk.Aanpassen(NBreuk);
+                SBreuk = NBreuk;
+            }
+            Console.Clear();
+            SBreuk.Get(SBreuk);
+            Console.Write("Wilt u iets aanpassen? (Y/N) ");
+            strAnswer = Console.ReadLine().ToUpper();
+
+            if (strAnswer == "Y")
+            {
+                SBreuk.Aanpassen(SBreuk);
+            }
+            Console.Clear();
+            dblQuotiënt = SBreuk.quotiënt(SBreuk);
+            Console.WriteLine($"Het quotiënt van {SBreuk.INTTELLER}/{SBreuk.INTNOEMER} is {dblQuotiënt}");
+            Console.ReadLine();
         }
     }
 }
